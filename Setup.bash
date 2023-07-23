70a776af892aeae979cde9a2fe2fef9e05ffa377
Nathan Fassnacht <57467493+Nathdoesmath@users.noreply.github.com>
2023-07-23 17:59:20 -0400

    Add bash arguments

--- Setup.bash
+++ Setup.bash
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+while getopts t:i:g:c:l:h: option
+do
+	case "${option}"
+		in
+		t)
+			token = ${option};;
+		i)
+			clientId = ${option};;
+		g)
+			guildID = ${option};;
+		c)
+			condName = ${option};;
+		l)
+			condLoc = ${option};;
+		h)
+			echo 
+"|----------------------------------------------------
+| Help
+|
+|	-h		Ouputs the help message
+|
+|	-t		Specifies the Discord bot token
+|
+|	-i 		Specefies the Discord bot application ID
+|
+|	-g		Specefies the ID of the test server
+|
+|	-c 		Specifies the Conda environment name
+|
+|	-l		Specifies the Conda environment file path
+|----------------------------------------------------"
+			exit;;
+	esac
+done
+		
+
 # ----------------------------------
 # Determine if the necessary packages are present
 if command -v node &> /dev/null; then
